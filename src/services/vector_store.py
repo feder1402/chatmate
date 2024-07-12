69fc20ecaca66e0c8775da7e4154f6dbf8ad8f37
Federico Raggi <federicoraggi@hotmail.com>
2024-07-11 19:28:20 -0700

    Migrated Chroma imports to langchain .0.0.22

--- src/services/vector_store.py
+++ src/services/vector_store.py
@@ -5,7 +5,7 @@ from langchain_community.document_loaders.text import TextLoader
 from langchain_community.document_loaders import UnstructuredMarkdownLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_experimental.text_splitter import SemanticChunker
-from langchain.vectorstores import Chroma 
+from langchain_community.vectorstores import Chroma
 from langchain_openai.embeddings import OpenAIEmbeddings
 
 msg = st.toast("Loading documents...")
