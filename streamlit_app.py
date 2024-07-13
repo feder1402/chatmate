868eff0d7e4e31266e47a434568108a27c8c6799
Federico Raggi <federicoraggi@hotmail.com>
2024-07-12 18:02:18 -0700

    Added sqllite3 dependency for cloud deployments

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
-# __import__('pysqlite3')
-# import sys
-# sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 st.set_page_config(layout="wide")
 
