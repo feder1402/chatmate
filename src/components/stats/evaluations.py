1b08bb7196d4f0ab5fc83a8d0474c9861ed28f67
Federico Raggi <federicoraggi@hotmail.com>
2024-07-23 13:20:46 -0700

    Fixed issue with Ragas library that removed context_relevancy metric

--- src/components/stats/evaluations.py
+++ src/components/stats/evaluations.py
@@ -4,7 +4,6 @@ from datasets import Dataset
 from ragas import evaluate
 from ragas.metrics import (
     answer_relevancy,
-    context_relevancy,
     faithfulness
 )
 
@@ -16,10 +15,9 @@ def evaluations(response, query):
     }
     dataset = Dataset.from_dict(data)
     with st.spinner("Evaluating..."):
-        evals = evaluate(dataset, metrics=[answer_relevancy, context_relevancy, faithfulness])
+        evals = evaluate(dataset, metrics=[answer_relevancy, faithfulness])
         evals_df = [
             ["answer_relevancy", f'{100*evals["answer_relevancy"]:0.0f}%', 'answer vs. question','Calculates the mean cosine similarity of the original question to a number of artifical questions generated from the answer'], 
-            ["context_relevancy", f'{100*evals["context_relevancy"]:0.0f}%', 'context vs. question','Calculates the % of sentences in the context that are relevant for answering the question'], 
             ["faithfulness", f'{100*evals["faithfulness"]:0.0f}%', ' answer vs. context', 'Claims in the answer are identified and cross checked against the context to determine if they can be inferred from it']
             ]
         st.dataframe(evals_df, hide_index=True, column_config={1:"metric", 2:"value", 3:"compares", 4:"how is calculated"})
